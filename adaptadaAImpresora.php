04d7bcae6a803d837bdc676c2d1fc7cd0fd5a93a
Jacob <jake_76847@hotmail.com>
2025-01-06 09:30:06 -0400

    update

--- adaptadaAImpresora.php
+++ adaptadaAImpresora.php
@@ -52,6 +52,9 @@ function escribir_log($mensaje, $archivo = "printer.log")
     }
 }
 
+$host = $_GET['host'];
+$pdfname = $_GET['pdfname'];
+
 $datos_pago_print = time() . ' | ' . $_GET['datos']['pagos_id'] . ' | ' . $_SERVER['REMOTE_ADDR'] . '\n';
 escribir_log($datos_pago_print);
 
@@ -80,72 +83,13 @@ $connector = new WindowsPrintConnector($printerPort);
 $printer = new Printer($connector);
 
 try {
-    // Datos principales
-    $printer->setJustification(Printer::JUSTIFY_CENTER);
 
-    // $success_icon = EscposImage::load('images/success-icon.jpg', false);
-    // $printer->bitImage($success_icon);
-    // $logo = EscposImage::load('images/Logo-Bambolina-Nuevo.jpg', false);
-    // $printer->bitImage($logo);
-
-    $printer->text("Bambolina - Escuela de Danza\n");
-    $printer->text("Recibo de Pago\n\n");
-    $printer->setJustification(Printer::JUSTIFY_LEFT);
-    $printer->text("Fecha: " . date("d/m/Y H:i:s", strtotime($datos['pagos_fecha'])) . "\n");
-    $printer->text("Alumno: " . $datos['clientes_nombre'] . "\n\n");
-
-    // Métodos de pago
-    $printer->text("Método de Pago:\n");
-    foreach ($metodos_usados as $metodo) {
-        $printer->text($metodo['metodospago_nombre'] . ": Bs. " . $metodo['pagos_metodospago_monto'] . "\n");
-    }
-    $printer->text("\n");
-
-    // Detalles de paquetes
-    $printer->text("Detalles:\n");
-    foreach ($detalle_paquetes as $paquete) {
-        $printer->text($paquete['paquetes_nombre'] . " - Bs. " . $paquete['paquetes_precio'] . "\n");
-    }
-    $printer->text("\nTotal Pagado: Bs. " . $datos['pagos_monto'] . "\n");
-
-    // Fechas de inicio
-    $printer->text("\nInicio de Clases:\n");
-    foreach ($fechas_inicio as $paquete) {
-        $printer->text($paquete['paquetes_nombre'] . "\n");
-        $printer->text("Inicio: " . date("d/m/Y", strtotime($paquete['paquetes_clientes_fechainicio'])) . " - ");
-        $printer->text("Fin: " . date("d/m/Y", strtotime($paquete['paquetes_clientes_fechavencimiento'])) . "\n");
-    }
-
-    // Horarios
-    if (count($data) > 0) {
-        $printer->text("\nHorario:\n");
-        $detectar_duplicados = [];
-        foreach ($data as $dia) {
-            $printer->text(dia_semana($dia['dia']) . "\n");
-            foreach ($dia as $horario) {
-                if (!is_array($horario)) continue;
-                foreach ($horario as $clase) {
-                    if (!in_array($clase, $detectar_duplicados)) {
-                        $detectar_duplicados[] = $clase;
-                        $printer->text($clase['clases_nombre'] . " - " . $clase['hora_inicio'] . " a " . $clase['hora_fin'] . "\n");
-                    }
-                }
-            }
-        }
-    }
-
-    // Footer
-    $printer->setJustification(Printer::JUSTIFY_CENTER);
-    $printer->text("\nGracias por tu pago. ¡Disfruta tus clases!\n");
-    $printer->text("#ThePinkHouse\n");
-
-    // Corta el papel
-    $printer->cut();
     echo '
     <script type="text/javascript">
         window.onload = function() {
             // Espera 3 segundos antes de cerrar la ventana para asegurar que todo se cargue
             setTimeout(function() {
+                window.open("' . $host . '/archivos/recibos/' . $pdfname . '");
                 window.close();
             }, 3000); // 3000 milisegundos = 3 segundos
         };
